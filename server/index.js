081aba30ff77b092b01f5419518ccc9f0d8865df
Nez <nez@Nezs-MBP.attlocal.net>
2017-09-30 14:21:36 -0700

    init/setup env variables & add procfile

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 1337;
+const port = process.env.PORT || 1337;
 
 app.use(express.static(path.resolve(__dirname, '../client/dist')));
 
