0f01e573bb7ef74c9e5d6dfe8033797715a3ca6e
Tiffany Pham <tiffanynpham@gmail.com>
2017-10-03 16:13:11 -0700

    On click, render a new page based on selected restaurant

---
+++ client/src/components/customer/CustomerInfoForm.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CustomerInfoForm = () => (
+  <h2>NAME OF RESTAURANT</h2>
+);
+
+export default CustomerInfoForm;
--- client/src/components/customer/SelectedRestaurant.jsx
+++ client/src/components/customer/SelectedRestaurant.jsx
@@ -1,5 +1,24 @@
 import React from 'react';
+// import RestaurantLogo from './RestaurantLogo.jsx';
+import CustomerInfoForm from './CustomerInfoForm.jsx';
+import CustomerQueueInfo from './CustomerQueueInfo.jsx';
 
+class SelectedRestaurant extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      infoSubmitted: false,
+      selectRestaurant: false
+    }
+  }
 
+  render() {
+    return (
+      <div className="selected-restaurant">
+        {this.state.infoSubmitted === false ? <CustomerInfoForm /> : <CustomerQueueInfo />}
+      </div>
+    )
+  }
+}
 
 export default SelectedRestaurant;
