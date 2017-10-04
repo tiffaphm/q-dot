6727df196e489a13434186c29efdea175a51ce21
Tiffany Pham <tiffanynpham@gmail.com>
2017-10-04 10:24:34 -0700

    Changes to customer info form

--- client/src/components/customer/CustomerInfoForm.jsx
+++ client/src/components/customer/CustomerInfoForm.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 class CustomerInfoForm extends React.Component {
   constructor(props) {
     super(props);
+    this.submitCustomerInfo = this.submitCustomerInfo.bind(this)
     this.state = {
       customerName: '',
       customerMobile: '',
@@ -12,34 +13,40 @@ class CustomerInfoForm extends React.Component {
     }
   }
 
+  submitCustomerInfo() {
+    // send post request back to server
+    console.log('submitted info!');
+  }
+
   render() {
     return(
       <div className="customer-info-input-container">
         <div className="row">
-          <form className="col s12">
             <div className="row">
               <div className="input-field col s6">
                 <input id="first_name" type="text" className="validate" />
-                <label for="first_name" data-error="wrong" data-success="right">First Name</label>
+                <label htmlFor="first_name" data-error="wrong" data-success="right">First Name</label>
               </div>
               <div className="input-field col s6">
                 <input id="last_name" type="text" className="validate" />
-                <label for="last_name" data-error="wrong" data-success="right">Last Name</label>
+                <label htmlFor="last_name" data-error="wrong" data-success="right">Last Name</label>
               </div>
             </div>
             <div className="row">
               <div className="input-field col s12">
                 <input id="telephone" type="tel" className="validate" />
-                <label for="telephone" data-error="wrong" data-success="right">Phone Number</label>
+                <label htmlFor="telephone" data-error="wrong" data-success="right">Phone Number</label>
               </div>
             </div>
             <div className="row">
               <div className="input-field col s12">
                 <input id="email" type="email" className="validate" />
-                <label for="email" data-error="wrong" data-success="right">Email</label>
+                <label htmlFor="email" data-error="wrong" data-success="right">Email</label>
               </div>
             </div>
-          </form>
+            <div className="row">
+              <input type="submit" value="Add to Queue" />
+            </div>
         </div>
       </div>
     )
