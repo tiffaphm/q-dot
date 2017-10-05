1b559e588c4e7a74ca5346112df17995a040c8a5
Nez <nez@Nezs-MBP.attlocal.net>
2017-10-04 18:34:03 -0700

    Define methods accepted by server

--- server/index.js
+++ server/index.js
@@ -16,6 +16,7 @@ app.use(express.static(path.resolve(__dirname, '../client/dist')));
 
 app.use((req, res, next) => {
   res.set('Access-Control-Allow-Origin', '*');
+  res.set('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS');
   next();
 });
 
