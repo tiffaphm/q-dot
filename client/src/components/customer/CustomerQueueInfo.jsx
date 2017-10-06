a4efa4381290419ba963cf9e9b0aedc25cbdbecf
Tiffany Pham <tiffanynpham@gmail.com>
2017-10-06 00:58:53 -0700

    Shell of component to display queue info

--- client/src/components/customer/CustomerQueueInfo.jsx
+++ client/src/components/customer/CustomerQueueInfo.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import $ from 'jquery';
 
-const CustomerQueueInfo = () => (
-  <p>(>^.^)> Hello World from /queueInfo!</p>
-);
+class CustomerQueueInfo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+
+    return (
+      <div className="customer-queue-info-container">
+        <div className="queue-divider"></div>
+          <h4>YOUR CURRENT POSITION IS</h4>
+      </div>
+    )
+  }
+}
 
 export default CustomerQueueInfo;
