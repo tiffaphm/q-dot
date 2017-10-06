24ee221a2ce88d2fd9a36c288706ef3185902e0e
Tiffany Pham <tiffanynpham@gmail.com>
2017-10-05 22:48:23 -0700

    Resolve merge conflict with customer nav

--- client/src/components/customer/CustomerNav.jsx
+++ client/src/components/customer/CustomerNav.jsx
@@ -1,23 +1,6 @@
 import React from 'react';
 
 const CustomerNav = () => (
-<<<<<<< HEAD
-  <div className="customer-nav-bar">
-    <ul id="dropdown1" className="dropdown-content">
-      <li><a href="#!">one</a></li>
-      <li><a href="#!">two</a></li>
-      <li className="divider"></li>
-      <li><a href="#!">three</a></li>
-    </ul>
-    <nav>
-      <div className="nav-wrapper">
-        <ul className="nav-mobile hide-on-med-and-down">
-          <li><a className="dropdown-button" href="#!" data-activates="dropdown1">q.<i className="material-icons right">arrow_drop_down</i></a></li>
-        </ul>
-      </div>
-    </nav>
-  </div>
-=======
     <div className="customer-nav-bar">
       <ul id="dropdown1" className="dropdown-content">
         <li><a href="/">home</a></li>
@@ -32,7 +15,6 @@ const CustomerNav = () => (
         </div>
       </nav>
     </div>
->>>>>>> click to show manager page
 );
 
 export default CustomerNav;
