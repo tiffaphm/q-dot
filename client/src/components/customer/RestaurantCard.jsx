ff32eaefdfc770e9b2a2f610aeb292e3b6d0ca82
Nez <nez@Nezs-MBP.attlocal.net>
2017-10-07 11:25:26 -0700

    Fix merge conflict in managerApp.jsx

--- client/src/components/customer/RestaurantCard.jsx
+++ client/src/components/customer/RestaurantCard.jsx
@@ -35,7 +35,7 @@ class RestaurantCard extends React.Component {
             </div>
             <div className="card-content">
               <span className="queue-number">groups in queue: {this.props.restaurant.queue_count} </span>
-              <span className="wait-time">wait time: {this.props.restaurant.total_wait} mins</span>
+              <span className="wait-time">wait time: {this.props.restaurant.total_wait - this.props.restaurant.average_wait} mins</span>
             </div>
           </div>
         </div>
---
+++ client/src/components/manager/AddToQueue.jsx
@@ -0,0 +1,82 @@
+import React from 'react';
+
+class AddToQueue extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      name: '',
+      mobile: '',
+      email: '',
+      size: 0
+    };
+  }
+
+  addToQueue (e) {
+    let queueInfo = {
+      name: this.state.name,
+      mobile: this.state.mobile,
+      size: this.state.size
+    };
+
+    if (this.state.email) {
+      queueInfo.email = this.state.email;
+    }
+
+    this.props.addCustomer(queueInfo);
+
+    this.resetValues();
+  }
+
+  getName (e) {
+    this.setState({name: e.target.value});
+  }
+
+  getMobile (e) {
+    this.setState({mobile: e.target.value});
+  }
+
+  getEmail (e) {
+    this.setState({email: e.target.value});
+  }
+
+  getSize (e) {
+    this.setState({size: e.target.value});
+  }
+
+  resetValues () {
+    this.state.name = '';
+    this.state.mobile = '';
+    this.state.size = 0;
+    this.state.email = '';
+  }
+
+  render () {
+    return (
+      <div className="nav navbar-nav">
+        <button type="button" className="btn btn-primary navbar-btn" data-toggle="modal" data-target="#add-to-queue">Add To Queue</button>
+        <div id="add-to-queue" className="modal fade" role="dialog">
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <button type="button" className="close" data-dismiss="modal">&times;</button>
+                <h2 className="modal-title">Customer Info</h2>
+              </div>
+              <div className="modal-body">
+                <label>Name: <input type="text" placeholder="name.." value={this.state.name} onChange={this.getName.bind(this)}/></label>
+                <label>Mobile: <input type="text" placeholder="mobile number.."value={this.state.mobile} onChange={this.getMobile.bind(this)}/></label>
+                <label>Email: <input type="text" placeholder="email.." value={this.state.email} onChange={this.getEmail.bind(this)}/></label>
+                <label>Size: <input type="text" placeholder="size of group.." value={this.state.size} onChange={this.getSize.bind(this)}/></label>
+              </div>
+              <div className="modal-footer">
+                <button className="btn btn-primary" data-dismiss="modal" onClick={this.addToQueue.bind(this)}>Confirm</button>
+                <button type="button" className="btn btn-default" data-dismiss="modal">Cancel</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AddToQueue;
