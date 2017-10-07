66dbc0e4cc9d48890e2260f540a22254591176eb
Nez <nez@Nezs-MBP.attlocal.net>
2017-10-07 11:22:01 -0700

    complete add to queue button, css fixes pending

--- client/src/components/manager/CustomerListEntry.jsx
+++ client/src/components/manager/CustomerListEntry.jsx
@@ -20,15 +20,14 @@ const CustomerListEntry = (props) => {
             <div className="modal-content">
               <div className="modal-header">
                 <button type="button" className="close" data-dismiss="modal">&times;</button>
-                <h2 className="modal-title">Statement</h2>
               </div>
               <div className="modal-body">
-                <p className="warning-content"><b>Remove</b> First Customer In Queue</p>
+                <p className="warning-content">Reason to remove from queue?</p>
               </div>
               <div className="modal-footer">
-                <button className="btn btn-warning" data-dismiss="modal" onClick={() => props.removeCustomer(props.queue.id)}>Customer Not Shown Up</button>
-                <button className="btn btn-success" data-dismiss="modal" onClick={() => props.removeCustomer(props.queue.id)}>Customer Here</button>
-                <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
+                <button className="btn btn-warning" data-dismiss="modal" onClick={() => props.removeCustomer(props.queue.id)}>No Show</button>
+                <button className="btn btn-success" data-dismiss="modal" onClick={() => props.removeCustomer(props.queue.id)}>Seated</button>
+                <button type="button" className="btn btn-default" data-dismiss="modal">Cancel</button>
               </div>
             </div>
           </div>
